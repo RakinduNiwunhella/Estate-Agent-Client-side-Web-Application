5608d84566e52287b056bd57039b816447dc9a83
Rakindu Niwunhella <rakindu15@gmail.com>
2025-12-19 21:52:36 +0530

    Add styled header component with logo and navigation
    Introduces a new Header component featuring a top info bar, logo, navigation links, and a favorite button. Adds related CSS for styling and imports Material Icons fonts. Updates App.jsx to render the Header and adds a logo image asset.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import Header from './components/Header'
 import './App.css'
 
 function App() {
   
 
   return (
-    <p>HI</p>
+
+    <Header/>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header-wrapper">
+      
+      {/* Top info bar */}
+      <div className="top-bar">
+        <div className="top-left">
+        <span className="material-icons top-bar-icon">phone</span>
+        <span>+94 070 225 2257</span>
 
-export default Header
+        <span className="material-icons top-bar-icon">email</span>
+        <span >sterlingproperties@gmail.com</span>
+        </div>  
+        <div className="top-right">
+          Open Mon-Fri 9am-6pm
+        </div>
+      </div>
+
+      {/* Main navbar */}
+      <div className="main-nav">
+        <div className="logo-section">
+            <img className="logo" src="src/assets/Logo.webp" alt="Sterling Properties logo" />
+          <div ></div>
+          <div>
+            <h1>Sterling Properties</h1>
+            <p>Your Home, Our Priority</p>
+          </div>
+        </div>
+
+        <nav className="nav-links">
+          <a href="#">Search Properties</a>
+          <a href="#">About Us</a>
+          <a href="#">Our Services</a>
+          <a href="#">Contact</a>
+        </nav>
+
+        <button className="fav-btn">
+          <span className="material-symbols-outlined heart-icon">favorite</span>
+        </button>
+      </div>
+
+    </header>
+  );
+};
+
+export default Header;
