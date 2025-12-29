42e89ef1c31a61b2d335efd056f19d8e47a574e8
Rakindu Niwunhella <rakindu15@gmail.com>
2025-12-29 11:24:52 +0530

    added react tabs
    added react tabs in propertydetails.jsx to display the property long description, the floor plan and the google map.

--- src/components/PropertyDetails.jsx
+++ src/components/PropertyDetails.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./PropertyDetails.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 function PropertyDetails({ property }) {
   const allImages = [property.picture, ...property.images];
@@ -17,6 +19,22 @@ function PropertyDetails({ property }) {
 
   return (
     <div className="property-details-wrapper">
+
+      {/* PROPERTY TITLE & LOCATION */}
+      <div className="property-header-top">
+        <h1 className="property-title">
+          {property.type} with {property.bedrooms} Bedrooms
+        </h1>
+
+        <div className="property-actions">
+          <button className="icon-btn">♡</button>
+        </div>
+      </div>
+
+      <p className="property-location">
+        {property.location}
+      </p>
+
       {/* IMAGE GALLERY */}
       <div className="gallery">
         <div className="main-image">
@@ -40,20 +58,6 @@ function PropertyDetails({ property }) {
 
       {/* PROPERTY HEADER */}
       <div className="property-header">
-        <div className="property-header-top">
-          <h1 className="property-title">
-            {property.type} with {property.bedrooms} Bedrooms
-          </h1>
-
-          <div className="property-actions">
-            <button className="icon-btn">♡</button>
-            <button className="icon-btn">⤴</button>
-          </div>
-        </div>
-
-        <p className="property-location">
-          {property.location}
-        </p>
 
         <div className="property-price">
           £{property.price.toLocaleString()}
@@ -77,6 +81,57 @@ function PropertyDetails({ property }) {
             <span className="link">{property.tenure}</span>
           </div>
         </div>
+{/* PROPERTY TABS */}
+<div className="property-tabs">
+  <Tabs>
+
+    <TabList>
+      <Tab>Description</Tab>
+      <Tab>Floor Plan</Tab>
+      <Tab>Map</Tab>
+    </TabList>
+
+    {/* DESCRIPTION TAB */}
+    <TabPanel>
+      <div className="property-description">
+        <p
+          dangerouslySetInnerHTML={{
+            __html: property.description,
+          }}
+        />
+      </div>
+    </TabPanel>
+
+    {/* FLOOR PLAN TAB */}
+    <TabPanel>
+      <p>Floor plan will be shown here.</p>
+    </TabPanel>
+
+    {/* MAP TAB */}
+    <TabPanel>
+      <div className="property-map">
+        <iframe
+          title="Property location map"
+          width="100%"
+          height="400"
+          loading="lazy"
+          allowFullScreen
+          referrerPolicy="no-referrer-when-downgrade"
+          src={`https://www.google.com/maps?q=${encodeURIComponent(
+            property.location
+          )}&output=embed`}
+        />
+      </div> 
+         </TabPanel>
+
+  </Tabs>
+</div>
+
+
+
+
+
+
       </div>
     </div>
   );
